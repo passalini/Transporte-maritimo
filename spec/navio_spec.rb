b81a40a9bfde8bc5123a8a640954a9ac5f514fbe
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-11 16:51:53 -0300

    navio pode ter cargas embarcadas (spec)

--- spec/navio_spec.rb
+++ spec/navio_spec.rb
@@ -5,9 +5,9 @@ describe Navio do
     @porto1 = Porto.create! nome: "Porto1"
     @porto2 = Porto.create! nome: "Porto2"
 
-    navio = Navio.create! nome: 'Navio1', capacidade: '550'
-    navio.rotas.create porto: @porto1, data_chegada: Date.tomorrow
-    navio.rotas.create porto: @porto2, data_chegada: Date.today + 5.days
+    @navio = Navio.create! nome: 'Navio1', capacidade: '550'
+    @navio.rotas.create porto: @porto1, data_chegada: Date.tomorrow
+    @navio.rotas.create porto: @porto2, data_chegada: Date.today + 5.days
   end
 
   it 'criar um navio' do
@@ -24,6 +24,16 @@ describe Navio do
 
     navio.rotas.find_by_porto_id(@porto1.id).data_chegada.should == Date.tomorrow
     navio.rotas.find_by_porto_id(@porto2.id).data_chegada.should == Date.today + 5.days
+  end
+
+  it "pode ou nao ter cargas embarcadas" do 
+    agente = Agente.create! nome: "Derp"
+    carga = CargaEmbarcada.create! numero: 001, peso: 100, data_max_desembarque: Date.today + 15.days,
+                               porto: @porto1, agente: agente
+    @navio.carga_embarcadas << carga
+
+    navio = Navio.find_by_id(@navio.id)
+    navio.carga_embarcadas.should include carga
   end
 end
 

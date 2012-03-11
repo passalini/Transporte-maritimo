26fe87e0bf47597a8e9bf5449d01fa8a745379dc
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-10 22:30:38 -0300

    mudança na relação entre porto e navio

--- domain/navio.rb
+++ domain/navio.rb
@@ -1,6 +1,7 @@
 require File.join(File.dirname(File.expand_path(__FILE__)), '..', 'config', 'ar_helper')
 
 class Navio < ActiveRecord::Base
-	has_and_belongs_to_many :portos
+	has_many :rotas
+	has_many :portos, :through => :rotas
 end
 
--- domain/porto.rb
+++ domain/porto.rb
@@ -1,5 +1,5 @@
 require File.join(File.dirname(File.expand_path(__FILE__)), '..', 'config', 'ar_helper')
 
 class Porto < ActiveRecord::Base
-	has_and_belongs_to_many :navios
+	has_many :navios, :through => :rotas
 end

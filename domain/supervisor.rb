212c15cdca8151df4f459baca06327333099205b
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-12 11:29:23 -0300

    carga só é embarcada se navio tiver capacidade

--- domain/supervisor.rb
+++ domain/supervisor.rb
@@ -2,9 +2,11 @@ require File.join(File.dirname(File.expand_path(__FILE__)), '..', 'config', 'ar_
 
 class Supervisor
 	def embarcar (carga, navio)
-		atributos_de_carga = carga.instance_values["attributes"] 
-		atributos_de_carga["navio_id"] = navio.id
-		carga.destroy
-		navio.carga_embarcadas << CargaEmbarcada.create!(atributos_de_carga) 
+		if carga.peso <= navio.capacidade
+			atributos_de_carga = carga.instance_values["attributes"] 
+			atributos_de_carga["navio_id"] = navio.id
+			carga.destroy
+			navio.carga_embarcadas << CargaEmbarcada.create!(atributos_de_carga) 
+		end
 	end
 end
--- spec/embarcar_carga_spec.rb
+++ spec/embarcar_carga_spec.rb
@@ -1,20 +1,25 @@
 #encoding: utf-8
 require './spec/spec_helper'
 
-describe "embarcar carga" do
-	it "embarcar uma carga qualquer em um navio" do 
+describe "Embarcar carga" do
+	before do
 		agente = Agente.create! nome: "Derp"
-		porto = Porto.create! nome: "Porto Qualquer"
-		carga = Carga.create! numero: 001, peso: 100, data_max_desembarque: Date.today + 15.days,
-															 porto: porto, agente: agente
-		porto1 = Porto.create! nome: "Porto1"
-	    navio = Navio.create! nome: 'Navio1', capacidade: '550'
-	    navio.rotas.create porto: porto1, data_chegada: Date.tomorrow + 3.days
+		porto = Porto.create! nome: "Porto destino da carga"
+		@carga = Carga.create! numero: 001, peso: 100, data_max_desembarque: Date.today + 15.days,
+ 												  porto: porto, agente: agente
+		
+		porto1 = Porto.create! nome: "Porto qualquer"
+    @navio = Navio.create! nome: 'Navio1', capacidade: '550'
+    @navio.rotas.create porto: porto1, data_chegada: Date.tomorrow + 3.days
 
-	    supervisor = Supervisor.new
-
-	    supervisor.embarcar(carga,navio)
-
-	    navio.carga_embarcadas.should include CargaEmbarcada.find_by_numero(carga.numero)
+    @supervisor = Supervisor.new
 	end
+
+	it "deve ser possivel se não esceder a capacidade do navio" do 
+		@navio.capacidade = 0
+		
+		@supervisor.embarcar(@carga,@navio)
+		@navio.carga_embarcadas.should_not include CargaEmbarcada.find_by_numero(@carga.numero)
+	end
+	
 end

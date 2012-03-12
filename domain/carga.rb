3d367147e4075f3d0bbb51d8373cec653f591f45
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-12 15:02:17 -0300

    carga embarca a si mesma no navio

--- domain/carga.rb
+++ domain/carga.rb
@@ -3,4 +3,10 @@ require File.join(File.dirname(File.expand_path(__FILE__)), '..', 'config', 'ar_
 class Carga < ActiveRecord::Base
 	belongs_to :porto
 	belongs_to :agente
+
+	def embarcar (navio)
+		if self.peso <= navio.capacidade
+			navio.cargas << carga
+		end
+	end
 end

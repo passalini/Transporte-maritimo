c3d55fd3034f92b3844a66cedccb0c3ba10fc28d
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-18 18:55:32 -0300

    navio tem portos de origem e destino (model)

--- domain/carga.rb
+++ domain/carga.rb
@@ -6,8 +6,8 @@ class Carga < ActiveRecord::Base
   belongs_to :agente
 
 	def embarcar (navio)
-		if self.peso <= navio.capacidade and navio.portos.include? self.porto_destino
-			navio.cargas << self if navio.viagens.find_by_porto_destino_id(self.porto_destino.id).data_chegada <= self.data_max_desembarque
+		if self.peso <= navio.capacidade and navio.portos_destino.include? self.porto_destino and navio.portos_origem.include? self.porto_origem
+			navio.cargas << self if navio.data_de_chegada(self.porto_destino) <= self.data_max_desembarque
 		end
 	end
 end
--- domain/navio.rb
+++ domain/navio.rb
@@ -2,7 +2,14 @@ require File.join(File.dirname(File.expand_path(__FILE__)), '..', 'config', 'ar_
 
 class Navio < ActiveRecord::Base
 	has_many :viagens
-	has_many :portos, :through => :viagens, :source => :porto_destino
+  has_many :portos_destino, :through => :viagens, :source => :porto_destino
+	has_many :portos_origem, :through => :viagens, :source => :porto_origem
+
 	has_many :cargas
+
+  def data_de_chegada(porto)
+    self.viagens.find_by_porto_destino_id(porto.id).data_chegada 
+  end
+
 end
 

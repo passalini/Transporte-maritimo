52527b6bdc9c8086c3c094fae872faf397916551
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-18 15:33:19 -0300

    rota descontinuada, agora, Navio tem viagens

--- spec/navio_spec.rb
+++ spec/navio_spec.rb
@@ -6,8 +6,7 @@ describe Navio do
     @porto2 = Porto.create! nome: "Porto2"
 
     @navio = Navio.create! nome: 'Navio1', capacidade: '550'
-    @navio.rotas.create porto: @porto1, data_chegada: Date.tomorrow
-    @navio.rotas.create porto: @porto2, data_chegada: Date.today + 5.days
+    @navio.viagens.create porto_origem: @porto1, porto_destino: @porto2, data_chegada: Date.tomorrow
   end
 
   it 'criar um navio' do
@@ -16,24 +15,23 @@ describe Navio do
     navio.should_not be_nil
     navio.nome.should == 'Navio1'
     navio.capacidade.should == 550
-    navio.portos.should include @porto1 and @porto2
+    navio.portos.should include @porto2
   end
 
-  it "deve conter um porto e data de chegada no mesmo" do 
+  it "deve fazer viagens de um porto origem para um destino com a data de chegada no mesmo" do 
     navio = Navio.find_by_nome 'Navio1'
 
-    navio.rotas.find_by_porto_id(@porto1.id).data_chegada.should == Date.tomorrow
-    navio.rotas.find_by_porto_id(@porto2.id).data_chegada.should == Date.today + 5.days
+    navio.viagens.find_by_porto_destino_id(@porto2.id).data_chegada.should == Date.tomorrow
   end
 
   it "pode ou nao ter cargas embarcadas" do 
     agente = Agente.create! nome: "Derp"
-    carga = CargaEmbarcada.create! numero: 001, peso: 100, data_max_desembarque: Date.today + 15.days,
-                               porto: @porto1, agente: agente
-    @navio.carga_embarcadas << carga
+    carga = Carga.create! numero: 001, peso: 100, data_max_desembarque: Date.today + 15.days,
+                               porto: @porto2, agente: agente
+    @navio.cargas << carga
 
     navio = Navio.find_by_id(@navio.id)
-    navio.carga_embarcadas.should include carga
+    navio.cargas.should include carga
   end
 end
 

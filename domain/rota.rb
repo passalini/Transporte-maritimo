deba77df06007e8bc49d01f8c80538d401695fb2
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-18 15:33:32 -0300

    delete rota

--- domain/rota.rb
+++
@@ -1,8 +0,0 @@
-require File.join(File.dirname(File.expand_path(__FILE__)), '..', 'config', 'ar_helper')
-
-class Rota < ActiveRecord::Base
-	belongs_to :navio
-	belongs_to :porto
-
-	validates_presence_of :porto, :data_chegada
-end

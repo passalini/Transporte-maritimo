a0e5070350995b4a8ae1759cb632d29d575c5cdc
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-19 18:11:33 -0300

    add factories novos

--- spec/factories.rb
+++ spec/factories.rb
@@ -15,4 +15,16 @@ FactoryGirl.define do
   factory :agente do
     sequence (:nome) { |n| "agente #{n}"}
   end
+
+  factory :viagem do
+    sequence (:data_chegada) {|n| Date.today + n.days}
+    porto_origem {FactoryGirl.create :porto}
+    porto_destino {FactoryGirl.create :porto}
+    navio {FactoryGirl.create :navio}
+  end
+
+  factory :navio do
+    sequence (:nome) { |n| "Navio #{n}"}
+    capacidade 1000 
+  end
 end

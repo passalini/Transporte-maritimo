024351e758b8997575f9dce7fd2f67d9d0ea56c0
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-19 11:19:02 -0300

    add Pluralização

--- config/ar_helper.rb
+++ config/ar_helper.rb
@@ -1,5 +1,13 @@
 require 'active_record'
 require 'yaml'
+require 'active_support/all'
+
+ActiveSupport::Inflector.inflections do |inflect|
+inflect.plural /m$/i, 'ns'
+inflect.singular /ns$/i, 'm'
+# inflect.plural /el$/i, 'eis'
+# inflect.singular /eis$/i, 'el'
+end
 
 ActiveRecord::Base.establish_connection(YAML::load(
   File.open(File.join(File.dirname(__FILE__), 'database.yml'))))

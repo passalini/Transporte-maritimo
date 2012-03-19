36fedb71b2c596432c94386b089c715becc23b54
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-19 18:23:37 -0300

    Should para mais de um navio em navio.relatorio

--- spec/relatorio_spec.rb
+++ spec/relatorio_spec.rb
@@ -13,6 +13,12 @@ describe "Relatorio" do
     it "deve informar nome, capacidade máxima, relação de portos(nome) de sua rota, com a data de chegada em cada porto destino." do
       Navio.relatorio.should == [{nome: "Navio 1", capacidade: 1000, portos: @navio.portos_data}]
 
+      navio2 = Factory.create :navio
+      Factory.create :viagem, navio: navio2
+      4.times{|i| Factory.create :viagem, navio: navio2, porto_origem: Porto.all.last}
+      
+      Navio.relatorio.should == [{nome: "Navio 1", capacidade: 1000, portos: @navio.portos_data},
+                                 {nome: "Navio 2", capacidade: 1000, portos: navio2.portos_data}]
     end
   end
 end

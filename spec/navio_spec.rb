4f31758d6adcb98978db636209ebb961fb5cba5c
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-18 21:20:52 -0300

    refatoração de testes

--- spec/navio_spec.rb
+++ spec/navio_spec.rb
@@ -9,15 +9,6 @@ describe Navio do
     @navio.viagens.create porto_origem: @porto1, porto_destino: @porto2, data_chegada: Date.tomorrow
   end
 
-  it 'criar um navio' do
-    navio = Navio.find_by_nome 'Navio1'
-
-    navio.should_not be_nil
-    navio.nome.should == 'Navio1'
-    navio.capacidade.should == 550
-    navio.portos.should include @porto2
-  end
-
   it "deve fazer viagens de um porto origem para um destino com a data de chegada no mesmo" do 
     navio = Navio.find_by_nome 'Navio1'
 
@@ -27,7 +18,7 @@ describe Navio do
   it "pode ou nao ter cargas embarcadas" do 
     agente = Agente.create! nome: "Derp"
     carga = Carga.create! numero: 001, peso: 100, data_max_desembarque: Date.today + 15.days,
-                               porto: @porto2, agente: agente
+                               porto_destino: @porto2, agente: agente
     @navio.cargas << carga
 
     navio = Navio.find_by_id(@navio.id)

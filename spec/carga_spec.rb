0cc59fd0dcef142a321830d08b9d46174a01df52
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-19 09:45:47 -0300

    Dados que a carga devem ter

--- spec/carga_spec.rb
+++ spec/carga_spec.rb
@@ -1,5 +1,11 @@
 require './spec/spec_helper'
 
 describe Carga do
+  it { should_not have_valid(:numero).when(nil) }
+  it { should_not have_valid(:peso).when(nil) }
+  it { should_not have_valid(:data_max_desembarque).when(nil) }
+  it { should_not have_valid(:porto_origem).when(nil) }
+  it { should_not have_valid(:porto_destino).when(nil) }
+  it { should_not have_valid(:agente).when(nil) }
 
 end

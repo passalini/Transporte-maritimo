219924dcfa349a8707b243e6ab21b640a0d6dd9d
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-21 16:15:55 -0300

    relação entre Portos e navios

--- domain/porto.rb
+++ domain/porto.rb
@@ -1,5 +1,6 @@
 require File.join(File.dirname(File.expand_path(__FILE__)), '..', 'config', 'ar_helper')
 
 class Porto < ActiveRecord::Base
-	has_many :navios, :through => :rotas
+  has_many :viagens, :foreign_key => "porto_destino_id"
+	has_many :navios, :through => :viagens
 end

eb2a45970b51d3876721cdb64326d8541a948831
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-21 17:49:35 -0300

    agente tem cargas (spec)

--- spec/agente_spec.rb
+++ spec/agente_spec.rb
@@ -4,4 +4,11 @@ require './spec/spec_helper'
 describe Agente do
   it { should_not have_valid(:nome).when(nil) }
   it { should_not have_valid(:porto).when(nil) }
+
+  it "pode ter nenhuma ou muitas cargas" do 
+    agente = Factory.create :agente
+    carga = Factory.create :carga
+
+    agente.cargas.should include carga
+  end
 end

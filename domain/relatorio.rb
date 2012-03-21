c2164d75ea9ae537b1318e3ead9d572b7aefacd1
Pedro Henrique <henrique.avulso@gmail.com>
2012-03-21 19:07:27 -0300

    Refatoração do todos metodos do Relatorio (meio tarde, eu sei)

--- domain/relatorio.rb
+++ domain/relatorio.rb
@@ -2,13 +2,8 @@ require File.join(File.dirname(File.expand_path(__FILE__)), '..', 'config', 'ar_
 
 class Relatorio
   def self.cargas_embarcadas
-    cargas_embarcadas = []
-    cargas = Carga.embarcadas
-    cargas.each do |carga| 
-      cargas_embarcadas << {carga: carga, navio: carga.navio, 
-                            data_chegada_navio: carga.navio.data_de_chegada(carga.porto_origem, carga.porto_destino)} 
-    end
-    cargas_embarcadas
+    Carga.all.map {|carga| {carga: carga, navio: carga.navio, 
+                            data_chegada_navio: carga.navio.data_de_chegada(carga.porto_origem, carga.porto_destino)}  }
   end
 
   def self.cargas_nao_embarcadas
@@ -16,38 +11,24 @@ class Relatorio
   end
 
   def self.navios
-    navios = [] 
-    Navio.all.each do |navio|
-      navios.push ({navio: navio , portos: navio.portos_e_data_de_chegada}) unless navio.em_manutencao?
-    end
-    navios
+    Navio.all.map {|navio| {navio: navio , portos: navio.portos_e_data_de_chegada} unless navio.em_manutencao?}
   end
 
   def self.navios_em_manutencao
-    manutencao = []
-    Navio.all.each do |navio|
-      manutencao.push navio if navio.em_manutencao?
-    end
-    manutencao
+    Navio.all.select{|navio| navio.em_manutencao?}
   end
 
   def self.cargas_que_nao_tem_como_embarcar
     todos_os_portos_destino = []
     Navio.all.map{|navio| todos_os_portos_destino.concat navio.portos_destino}
-    cargas = Carga.nao_embarcadas
-
-    cargas.delete_if{|carga| todos_os_portos_destino.include? carga.porto_destino }
+    Carga.nao_embarcadas.delete_if{|carga| todos_os_portos_destino.include? carga.porto_destino }
   end
 
   def self.portos
-    portos_navios = []
-    Porto.all.map { |porto| portos_navios << {porto => porto.navios} }
-    portos_navios
+    Porto.all.map { |porto| {porto => porto.navios} }
   end
 
   def self.portos_agentes_sem_carga
-    portos_navios = []
-    Porto.all.map { |porto| portos_navios << {porto => porto.agentes.delete_if{|agente| not agente.cargas.empty?} } }
-    portos_navios
+    Porto.all.map { |porto| {porto => porto.agentes.delete_if{|agente| not agente.cargas.empty?} } }.delete_if{|porto| porto.values.first.empty?}
   end
 end
